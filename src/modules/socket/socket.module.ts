623beb98b12403e6531c772a97a3cd53bb44304b
nguyenlyminhman <nguyenlyminhman@gmail.com>
2026-04-20 22:53:10 +0700

    init modules

---
+++ src/modules/socket/socket.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { SocketController } from './socket.controller';
+import { SocketService } from './socket.service';
+
+@Module({
+  controllers: [SocketController],
+  providers: [SocketService]
+})
+export class SocketModule {}
