8ca104d7e3461fba4e45f5700881f182611b2267
nguyenlyminhman <nguyenlyminhman@gmail.com>
2026-04-26 23:46:35 +0700

    config project

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './modules/chat/chat.module';
 import { SocketModule } from './modules/socket/socket.module';
+import { DbConnectModule } from './modules/db-connect/db-connect.module';
+import { McpCvModule } from './modules/mcp-cv/mcp-cv.module';
+import { McpGithubModule } from './modules/mcp-github/mcp-github.module';
+import { McpChatHistoryModule } from './modules/mcp-chat-history/mcp-chat-history.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ChatModule, SocketModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      // load: [() => ({
+      //   defaultLang: 'vi',
+      // })],
+    }),
+    ChatModule, 
+    SocketModule, 
+    DbConnectModule, 
+    McpCvModule, 
+    McpGithubModule, 
+    McpChatHistoryModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
