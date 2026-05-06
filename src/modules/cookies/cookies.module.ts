5f1509f5dc5d8097131d981e5f1e577233f040ec
nguyenlyminhman <nguyenlyminhman@gmail.com>
2026-05-06 22:45:24 +0700

    init cookies

--- src/modules/cookies/cookies.module.ts
+++ src/modules/cookies/cookies.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { CookiesController } from './cookies.controller';
 import { CookiesService } from './cookies.service';
 import { DbConnectModule } from '../db-connect/db-connect.module';
+import { ConversationService } from '../conversation/conversation.service';
 
 @Module({
   imports: [DbConnectModule],
   controllers: [CookiesController],
-  providers: [CookiesService],
+  providers: [
+    CookiesService, 
+    ConversationService
+  ],
 })
 export class CookiesModule {}
--- src/modules/cookies/cookies.service.ts
+++ src/modules/cookies/cookies.service.ts
@@ -1,17 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { v4 as uuidv4 } from 'uuid';
 import { DbConnectService } from '../db-connect/db-connect.service';
+import { ConversationService } from '../conversation/conversation.service';
 
 @Injectable()
 export class CookiesService {
     constructor(
-        private readonly db: DbConnectService
-    ) {}
+        private readonly db: DbConnectService,
+        private readonly conversationService: ConversationService
+    ) { }
 
     async createSessionId(): Promise<string> {
         const rs = uuidv4().toString();
 
-        await this.db.hr_sessions.create({ data: { id: uuidv4(), cookie_token: rs , ip_address: '' } });
+        await this.db.hr_sessions.create({
+            data: {
+                id: uuidv4(),
+                cookie_token: rs,
+                user_agent: rs,
+                ip_address: '',
+                first_seen_at: new Date(),
+                last_seen_at: new Date(),
+            }
+        });
+
+        await this.conversationService.createConversation(rs);
+
         return rs;
     }
 }
