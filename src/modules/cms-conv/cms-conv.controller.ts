ab97dcbe7813b396ac074407e7a986b4827d1788
nguyenlyminhman <nguyenlyminhman@gmail.com>
2026-05-11 14:45:39 +0700

    format code

--- src/modules/cms-conv/cms-conv.controller.ts
+++ src/modules/cms-conv/cms-conv.controller.ts
@@ -4,4 +4,4 @@ import { EApiPath, VERSION_1 } from 'src/objects/enum/EApiPath.enum';
 
 @ApiTags('Cms Conv')
 @Controller({ path: EApiPath.CMS_CONV, version: VERSION_1 })
-export class CmsConvController {}
+export class CmsConvController { }
--- src/modules/cms-conv/cms-conv.service.ts
+++ src/modules/cms-conv/cms-conv.service.ts
@@ -4,24 +4,27 @@ import { DbConnectService } from '../db-connect/db-connect.service';
 
 @Injectable()
 export class CmsConvService {
-    constructor(
-        private readonly db: DbConnectService
-    ) { }
+  constructor(
+    private readonly db: DbConnectService
+  ) { }
 
-    async createConversation(sessionId: string): Promise<string> {
-        const rs = await this.db.conversations.create({
-            data: {
-                id: uuidv4(),
-                session_id: sessionId,
-                started_at: new Date(),
-                last_message_at: new Date(),
-                message_count: 0,
-            },
-            select: {
-                id: true,
-            }
-        });
+  async createConversation(sessionId: string): Promise<string> {
+    const rs = await this.db.conversations.create({
+      data: {
+        id: uuidv4(),
+        session_id: sessionId,
+        started_at: new Date(),
+        last_message_at: new Date(),
+        message_count: 0,
+      },
+      select: {
+        id: true,
+      }
+    });
 
-        return rs.id;
-    }
+    return rs.id;
+  }
+
+
+
 }
