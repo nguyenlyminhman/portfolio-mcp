168ae145fca27cecd4bbb7153ae075b02bd5531d
nguyenlyminhman <nguyenlyminhman@gmail.com>
2026-05-11 14:16:00 +0700

    change endpoint

--- src/objects/enum/EApiPath.enum.ts
+++ src/objects/enum/EApiPath.enum.ts
@@ -8,6 +8,6 @@ export enum EApiPath {
     CHAT = '/chat',
     HR_SESSION = '/hr-session',
     CMS = '/cms',
-    CMS_CV = '/cms-cv',
-    CMS_REPO = '/cms-repo'
+    CMS_CV = '/cms/cv',
+    CMS_REPO = '/cms/repo'
 }
