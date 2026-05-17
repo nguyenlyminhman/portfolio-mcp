0048d65c5ed0957e39bf2095443b20884f112a13
nguyenlyminhman <nguyenlyminhman@gmail.com>
2026-05-18 00:08:57 +0700

    002

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHealthCheck(): string {
+    console.log('check +>>>>>>>>>>>>>>>>>,', process.env.JWT_SECRET);
+    
     return 'Health check...';
   }
 }
