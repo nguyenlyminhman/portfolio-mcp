83448efe7021c5353f1d2fe7bd5685c2e7ba077b
nguyenlyminhman <nguyenlyminhman@gmail.com>
2026-05-17 14:59:35 +0700

    add health

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,12 +2,15 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Public } from './decorator/public.decorator';
 import { EApiPath, VERSION_1 } from './objects/enum/EApiPath.enum';
+import { ApiTags } from '@nestjs/swagger';
 
-@Public()
+
+@ApiTags('App')
 @Controller({ path: EApiPath.HEALTH, version: VERSION_1 })
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getHello(): string {
     return this.appService.getHealthCheck();
